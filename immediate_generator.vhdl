163b1e430f2e4553ed03b02a0b0bb0f9e8193270
georgeyork1 <george.york@afacademy.af.edu>
2025-07-07 15:53:28 -0600

    assignment

--- immediate_generator.vhdl
+++ immediate_generator.vhdl
@@ -35,12 +35,14 @@ begin
                 end if;                   
             when "1100011" => -- BNE
                 if instr(31) = '0' then  -- positive
-                    imm <= "0000000000000000000000" & instr(7) &  instr(30 downto 25) & instr(11 downto 9); -- div by 4, or shift left 2
+                    -- imm math points to a byte, not 16-bit or word
+                    imm <= "0000000000000000000000" & instr(7) &  instr(30 downto 25) & instr(11 downto 9); 
                 else -- negative
-                    imm <= "1111111111111111111111" & instr(7) &  instr(30 downto 25) & instr(11 downto 9); -- div by 4, or shift left 2
+                    imm <= "1111111111111111111111" & instr(7) &  instr(30 downto 25) & instr(11 downto 9); 
                 end if;                
             when "1101111" => -- J
                  if instr(31) = '0' then  -- positive
+                    -- imm math points to a byte, not 16-bit or word
                     imm <= "000000000000" & instr(31) & instr(21 downto 12) & instr(22) & instr(30 downto 23);
                 else -- negative
                     imm <= "111111111111" & instr(31) & instr(21 downto 12) & instr(22) & instr(30 downto 23);
